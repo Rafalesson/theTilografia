d8dc4355247b211c3a370721b90a0b44aae831a3
Rafalesson <rafaalesson35@gmail.com>
2022-02-13 20:42:54 -0300

    implementação de tempo

--- js/index.js
+++ js/index.js
@@ -1,29 +1,69 @@
-function mostraNumPalavras(){
+function mostraNumPalavras() {
     let frases = $('.campo-digitacao').text()
-    
-    let numPalavras = frases.split(' ').length-1
-    
+
+    let numPalavras = frases.split(/\S+/).length - 1
+
     let tamanhoFrase = $('#caracter')
 
     return tamanhoFrase.text(numPalavras)
 }
-mostraNumPalavras()
 
-function eventoClick (){
+
+function campoDigitacao() {
     let campo = $('.campo-digitacao')
-    let botao = $('.botao')
-    campo.on('input', function(){
-        
-    let conteudo = campo.val()
-        
-    let qtdPalavras = conteudo.split(/\S+/).length-1
-    $('#palavras').text(qtdPalavras)
+    campo.on('input', function () {
 
-    let qtdCaracter = conteudo.length
-    $('#caracter').text(qtdCaracter)
+        let conteudo = campo.val()
+
+        let qtdPalavras = conteudo.split(/\S+/).length - 1
+        $('#palavras').text(qtdPalavras)
+
+        let qtdCaracter = conteudo.length
+        $('#caracter').text(qtdCaracter)
 
     })
 }
 
-eventoClick()
-    
+campoDigitacao()
+
+function senhorDoTempo() {
+    let campo = $('.campo-digitacao')
+    let tempoRestante = $('.segundos').text()
+
+    campo.one('focus', function () {
+        let cronometro = setInterval(function () {
+            tempoRestante--
+            $('.segundos').text(tempoRestante)
+
+            if (tempoRestante == 3){
+
+            }
+
+            if(tempoRestante == 3){
+                $('.segundos').css({
+                    background: 'red',
+                    color: 'white'
+                })
+            }
+            
+            if (tempoRestante == 0) {
+                campo.attr('disabled', true)
+                clearInterval(cronometro)
+                $('.segundos').text(`REINICIAR`)
+                $('.campo-digitacao').css({
+                    background: 'white',
+                    fontWeight: 'bold'
+                })
+            }
+            
+        }, 1000)
+    })
+}
+
+senhorDoTempo()
+
+
+
+
+
+
