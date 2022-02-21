e8191e9062cd7e6a656408141e15045c4940a69a
Rafalesson <rafaalesson35@gmail.com>
2022-02-21 17:01:29 -0300

    theTilografia quase pronta. Só falta o responsivo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+let campo = $('.campo-digitacao')
+let tempoInicial = $('.segundos').text()
+
 function mostraNumPalavras() {
     let frases = $('.texto-box1').text()
 
@@ -12,7 +15,6 @@ mostraNumPalavras()
 
 
 function campoDigitacao() {
-    let campo = $('.campo-digitacao')
     campo.on('input', function () {
 
         let conteudo = campo.val()
@@ -25,11 +27,47 @@ function campoDigitacao() {
 
     })
 }
-
 campoDigitacao()
 
+function botaoRemover(){
+    $('.bt-remover').click(function(event){
+        event.preventDefault()
+        $(this).parent().parent().parent().remove()
+        
+    })
+}
+botaoRemover()
+
+function inserePlacar() {
+    let corpoTabela = $('.placar').find('tbody')
+    let usuario = 'Rafa Alesson'
+    let numPalavras = $('#palavras').text()
+   
+    let linha = novaLinha(usuario, numPalavras)
+    linha.find('.bt-remover').click(botaoRemover)
+    corpoTabela.append(linha)
+
+}
+
+function novaLinha(usuario, palavras){
+    let linha = $('<tr>')
+    let colunaUsuario = $('<td>').text(usuario)
+    let colunaPalavras = $('<td>').text(palavras)
+    let colunaRemover = $('<td>')
+    let link = $('<a>').attr('href', '#restart')
+    let icone = $('<img>').attr('src', '../img/lixo.png').addClass('bt-remover')
+
+    link.append(icone)
+    colunaRemover.append(link)
+    linha.append(colunaUsuario)
+    linha.append(colunaPalavras)
+    linha.append(colunaRemover)
+
+    return linha
+}
+
 function senhorDoTempo() {
-    let campo = $('.campo-digitacao')
+    campo = $('.campo-digitacao')
     let tempoRestante = $('.segundos').text()
 
     campo.one('focus', function () {
@@ -47,12 +85,18 @@ function senhorDoTempo() {
             if (tempoRestante == 0) {
                 clearInterval(cronometro)
                 campo.attr('disabled', true)
-                $('.segundos').text(`REINICIAR`)
+                $('.segundos').text(`Game Over`)
 
                 $('.campo-digitacao').css({
                     background: 'white',
                     fontWeight: 'bold'
                 })
+
+                $('.segundos').css({
+                    background: 'rgba(245, 250, 254)',
+                    color: 'red'
+                })
+
                 campo.removeClass('borda-vermelha')
                 campo.removeClass('borda-verde')
                 inserePlacar()
@@ -63,13 +107,26 @@ function senhorDoTempo() {
 }
 senhorDoTempo()
 
-
-
-
+function botaoRestart() {
+    $('#restart').click(function () {
+        campo.attr('disabled', false)
+        campo.val('')
+        campo.css({
+            background: 'rgba(255, 255, 255, .1)'
+        })
+        $('#palavras').text('0')
+        $('.segundos').text(tempoInicial)
+        $('.segundos').css({
+            color: 'black'
+        })
+        senhorDoTempo()
+    })
+}
+botaoRestart()
 
 function verificaTexto() {
     let texto = $('.texto-box1').text()
-    let campo = $('.campo-digitacao')
+    campo = $('.campo-digitacao')
 
     campo.on('input', function () {
         let digitado = campo.val()
@@ -88,15 +145,3 @@ function verificaTexto() {
 
 verificaTexto()
 
-function inserePlacar() {
-    let corpoTabela = $('.placar').find('tbody')
-    let usuario = 'Rafa Alesson'
-    let numPalavras = $('#palavras').text()
-
-    let linha = `<tr>
-        <td> ${usuario} </td>
-        <td> ${numPalavras} </td>
-    </tr>`
-
-    corpoTabela.append(linha)
-}
